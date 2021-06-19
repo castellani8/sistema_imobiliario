c3a7f043dd5f71458d6cc7bafd2c2feb104f1ade
Lucas Castellani <lucas100castellani@gmail.com>
2021-06-19 00:03:53 -0300

    Configuração mix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
-    <link rel="stylesheet" href="assets/css/reset.css"/>
-    <link rel="stylesheet" href="assets/css/boot.css"/>
-    <link rel="stylesheet" href="assets/css/login.css"/>
+    <link rel="stylesheet" href="{{ mix('backend/assets/css/reset.css') }}"/>
+    <link rel="stylesheet" href="{{ mix('backend/assets/css/boot.css') }}"/>
+    <link rel="stylesheet" href="{{ mix('backend/assets/css/login.css') }}"/>
     <link rel="icon" type="image/png" href="assets/images/favicon.png"/>
 
     <title>UpAdmin - Site Control</title>
@@ -54,7 +54,8 @@
 
 </div>
 
-<script src="assets/js/jquery.min.js"></script>
+<script src="{{ mix('resources/views/admin/assets/js/jquery.js') }}"></script>
+<script src="{{ mix('resources/views/admin/assets/js/login.js') }}"></script>
 
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,24 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix
+.sass('resources/views/admin/assets/scss/reset.scss', 'public/backend/assets/css/reset.css')
+.sass('resources/views/admin/assets/scss/boot.scss', 'public/backend/assets/css/boot.css')
+.sass('resources/views/admin/assets/scss/login.scss', 'public/backend/assets/css/login.css')
+
+.js('resources/views/admin/assets/js/jquery.min.js', 'resources/views/admin/assets/js/jquery.js')
+.js('resources/views/admin/assets/js/login.js', 'resources/views/admin/assets/js/login.js')
+
+.copyDirectory('resources/views/admin/assets/css/fonts', 'public/backend/assets/css/fonts')
+
+.copyDirectory('resources/views/admin/assets/images', 'public/backend/assets/images')
+
+
+.options({
+    processCssUrls: false
+})
+
+.version()
+;
+
