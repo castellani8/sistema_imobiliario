2174132817c6abc91d8fabf0e13a815ce6bcc9b4
Lucas Castellani <lucas100castellani@gmail.com>
2021-06-19 03:04:52 -0300

    Middleware configurado e atualizado, grupo de rotas configurado

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function(){
 
+    // Formulario de login
     Route::get('/', 'AuthController@showLoginForm')->name('login');
     Route::post('login', 'AuthController@login')->name('login.do');
 
-    Route::get('home', 'AuthController@home')->name('home');
+    // Rotas protegidas
+    Route::group(['middleware' => ['auth']], function(){
+
+        // Dashboard
+        Route::get('home', 'AuthController@home')->name('home');
+        
+    });
+   
+
+    // Logout
+    Route::get('logout', 'AuthController@logout')->name('logout');
 
 });
